80815d72f80e3c789b396ea6d8ba753147ec8018
lucas8 <luc.linux@mailoo.org>
2012-11-19 17:22:04 +0100

    Ajout des graphes FreePlane.

---
+++ pieuvre.mm
@@ -0,0 +1,78 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Lanceur de billes" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1353339807017"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ff0000" WIDTH="5" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_675029940" MIDDLE_LABEL="FP1 : Lancer" STARTINCLINATION="54;-49;" ENDINCLINATION="-12;37;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#0000ff" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1886619624" MIDDLE_LABEL="FC1 : Communiquer par WiFi" STARTINCLINATION="-9;59;" ENDINCLINATION="4;-49;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#0000ff" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_622853819" MIDDLE_LABEL="FC2 : Ne pas tirer sur un obstacle humain" STARTINCLINATION="-61;0;" ENDINCLINATION="112;-35;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ff0000" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_19229741" MIDDLE_LABEL="FP2 : Capter la distance" STARTINCLINATION="-50;-6;" ENDINCLINATION="120;16;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#0000ff" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_827614789" MIDDLE_LABEL="FC4 : Allumage / extinction" STARTINCLINATION="-25;-55;" ENDINCLINATION="60;69;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#0000ff" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1501981347" MIDDLE_LABEL="FC3 : Utiliser une source portable" STARTINCLINATION="69;0;" ENDINCLINATION="-119;-14;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Utilisateur" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_827614789" CREATED="1353338819697" MODIFIED="1353338902536" HGAP="29" VSHIFT="-140">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Billes" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_675029940" CREATED="1353338711044" MODIFIED="1353339115904" HGAP="246" VSHIFT="-100">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Cible" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_19229741" CREATED="1353338881563" MODIFIED="1353339499249" HGAP="-151" VSHIFT="-90">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="&#xc9;nergie" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1501981347" CREATED="1353338948579" MODIFIED="1353339809553" HGAP="309" VSHIFT="130">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="S&#xe9;curit&#xe9;" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_622853819" CREATED="1353338910155" MODIFIED="1353339813534" HGAP="-161" VSHIFT="130">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="T&#xe9;l&#xe9;phone android" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1886619624" CREATED="1353338922131" MODIFIED="1353339415856" HGAP="79" VSHIFT="180">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+</map>
